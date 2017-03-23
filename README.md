2af8e08775f927b1dbfe725e0fc27810282556f3
Aaron Kennedy <aaronkenn@yahoo.com>
2017-03-23 17:16:23 +1100

    initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# Collapsible Content Plugin
+
+Collapsible Contnet is a WordPress Plugin that shows and hides hidden content. Pracicle examples include, Q&A, FAQ, hints, marketing teasers and more.
+
+## Features 
+   
+This plugin includes the following features:
+
+- QA Shortcode, '[qa]'
+- Teaser Shortcode, '[teaser]'
+- Font icon visual indicator
+- Jquery sliding animation
+
+## Installation 
+
+To install this plugin, you can download it by clicking on the GitHub download button or clone the repository.
+
+1. Navigate to the 'wp-content/plugins' folder of your project.
+2. Clone the repository by copying /pasting this line of code: 'git clone git@github.com:aaronkenn/CollapsibleContent'.
+3. Log into your WordPress website. 
+4. Go to plugins and acivate the collapsible content plugin. 
+
+## Continue Development
+
+If you want to contnue development you will need to have Composer, Gulp, Node.js, and npm installed on your machine. 
+
+1. Navigate to the 'wp-content/plugins/ collapsible-content' folder. 
+2. Type 'npm-insall' to install of the 'node_modules' for Gulp.
+3. Type 'composer install' to insallthe Composer PHP packages.
+  
---
+++ bootstap.php
@@ -0,0 +1,63 @@
+<?php
+
+/*
+* Collapsible Content plugin
+
+* 
+
+* @package		RagingPixel\CollapsibleContent
+
+* @author		Aaron Kennedy 
+
+* @License		GNU-2.0+
+
+* @link			http://www.ragingpixel.com.au
+
+*	
+
+* @wordpress-plugin
+
+* Plugin Name: Collapsible Content
+
+* Plugin URI: _
+
+* Description: Collapsible Contnet is a WordPress Plugin that shows and hides hidden content. Pracicle examples include, Q&A, FAQ, hints, marketing teasers and more.
+
+* Version: 1.0.0
+
+* Author: Aaron Kennedy
+
+* Author URI: http://www.ragingpixel.com.au
+
+* Text Domain: collapsible_content
+
+* Requires WP: 4.7
+
+* Requires PHP: 5.5
+
+*/
+
+
+
+/*
+	This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+namespace RagingPixel\CollapsibleContent; 
+
+function plugin_launch(){
+	
+	
+}
