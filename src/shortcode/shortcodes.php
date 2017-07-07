ff727670bc05e5bbaf192078765727d5ad084255
Aaron Kennedy <aaronkenn@yahoo.com>
2017-07-07 16:45:45 +1000

    Built the html View files

---
+++ src/shortcode/shortcodes.php
@@ -0,0 +1,5 @@
+<?php
+
+//[qa question="What question would you like to ask me?" show_icon="" hide_icon=""]This is the answer[/qa]
+
+//[teaser visable_message="What question would you like to ask me?" show_icon=""]This is the answer[/teaser]
